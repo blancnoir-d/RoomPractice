9d8c1dc2f086319ee27a019d3d7d4a6022e3b039
Saeha <teisuishuyi@gmail.com>
2018-12-21 12:40:56 +0900

    start

---
+++ app/src/main/java/com/project/shis/roompractice/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.project.shis.roompractice
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
