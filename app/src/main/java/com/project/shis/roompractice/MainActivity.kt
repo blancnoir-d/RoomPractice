cc4e3f136f2b77a00905a5d2cd31257dacea6d30
Saeha <teisuishuyi@gmail.com>
2018-12-21 12:42:53 +0900

    androidx migrate

--- app/src/main/java/com/project/shis/roompractice/MainActivity.kt
+++ app/src/main/java/com/project/shis/roompractice/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.project.shis.roompractice
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
